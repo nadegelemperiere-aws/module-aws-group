79ecbbd2e8084743f1780360138ba263fb9bfd65
Nadege <nadege.lemperiere@gmail.com>
2023-12-12 22:57:44 -0500

    Adding missing outputs

--- output.tf
+++ output.tf
@@ -2,19 +2,22 @@
 # Copyright (c) [2022] Nadege Lemperiere
 # All rights reserved
 # -------------------------------------------------------
-# Module to deploy the initial permissions associated to
-# an AWS SSO account
+# Module to deploy an SSO group
 # -------------------------------------------------------
 # Nadège LEMPERIERE, @14 november 2021
-# Latest revision: 14 november 2021
+# Latest revision: 12 december 2023
 # -------------------------------------------------------
 
 output "name" {
-    value = aws_ssoadmin_permission_set.permission_set.name
+    value = aws_identitystore_group.group.name
 }
 
-output "instance_arn" {
-    value = aws_ssoadmin_permission_set.permission_set.instance_arn
+output "id" {
+    value = aws_identitystore_group.group.group_id
+}
+
+output "permissions" {
+    value = aws_ssoadmin_permission_set.permission_set.name
 }
 
 output "arn" {
